1928c34a4be58c1042a508d7705cbf519292ec36
Mallam Chaitanya <mallamchaitanya2005@gmail.com>
2025-08-04 07:08:13 +0530

    Time: 0 ms (100.00%) | Memory: 41.5 MB (17.82%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.java
+++ 213-house-robber-ii/house-robber-ii.java
@@ -1,25 +1,26 @@
 class Solution {
     public int rob(int[] nums) {
         int n=nums.length;
-        if(n<2) return nums[0];
-        int skipLastHouse[]=new int[n-1];
-        int skipFirstHouse[]=new int[n-1];
-        for(int i=0;i<n-1;i++){
-            skipLastHouse[i]=nums[i];
-            skipFirstHouse[i]=nums[i+1];
-        }
-        int lootskippingLast=robHelper(skipLastHouse,n-1);
-        int lootSkippingFirst=robHelper(skipFirstHouse,n-1);
-        return Math.max(lootskippingLast,lootSkippingFirst);
-    }
-    public int robHelper(int[] nums,int n) {
         if(n==0) return 0;
-        if (n==1) return nums[0];
+        if(n==1) return nums[0];
+        int skipFirst[]=new int[n-1];
+        int skipLast[]=new int[n-1];
+        for(int i=0;i<n-1;i++){
+            skipFirst[i]=nums[i+1];
+            skipLast[i]=nums[i];
+        }
+        int skipFirstLoot=robHelper(skipLast,n-1);
+        int skipLastLoot=robHelper(skipFirst,n-1);
+        return Math.max(skipFirstLoot,skipLastLoot);
+    }
+    public int robHelper(int nums[],int n){
+        if(n==0) return 0;
+        if(n==1) return nums[0];
         int dp[]=new int[n+1];
         dp[0]=nums[0];
         dp[1]=Math.max(dp[0],nums[1]);
         for(int i=2;i<n;i++){
-            dp[i]=Math.max(dp[i-2]+nums[i],dp[i-1]);
+            dp[i]=Math.max(dp[i-1],dp[i-2]+nums[i]);
         }
         return dp[n-1];
     }
