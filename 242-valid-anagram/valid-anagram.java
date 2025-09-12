659f97702067487fc4a35f3b65ffbba40b380ece
Mallam Chaitanya <mallamchaitanya2005@gmail.com>
2025-09-12 11:16:24 +0530

    Time: 2 ms (97.75%) | Memory: 44.6 MB (51.15%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -6,9 +6,9 @@ class Solution {
         char []str2=t.toCharArray();
         Arrays.sort(str1);
         Arrays.sort(str2);
-        for(int i = 0; i< str1.length; i++) {
-            if(str1[i] != str2[i]) return false;
-        }
-        return true;
+        String a=new String(str1);
+        String b=new String(str2);
+        if(a.equals(b)) return true;
+        return false;
     }
 }
