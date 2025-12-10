96f8ac0c469b48160535732e20f644f017638894
Mallam Chaitanya <mallamchaitanya2005@gmail.com>
2025-12-10 20:09:03 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (30.64%) - LeetSync

--- 1833-find-the-highest-altitude/find-the-highest-altitude.java
+++ 1833-find-the-highest-altitude/find-the-highest-altitude.java
@@ -4,7 +4,12 @@ class Solution {
         for(int i=1;i<res.length;i++){
             res[i]=gain[i-1]+res[i-1];
         }
-        Arrays.sort(res);
-        return res[res.length-1];
+        int maxNum=res[0];
+        for(int i=1;i<res.length;i++){
+            if(res[i]>maxNum){
+                maxNum=res[i];
+            }
+        }
+        return maxNum;
     }
 }
