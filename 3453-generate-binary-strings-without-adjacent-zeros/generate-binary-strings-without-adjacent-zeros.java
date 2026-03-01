359413827e97ca9dbf38435745e41410ee6493b6
Mallam Chaitanya <mallamchaitanya2005@gmail.com>
2026-03-01 21:22:38 +0530

    Time: 64 ms (5.58%) | Memory: 47.8 MB (53.96%) - LeetSync

---
+++ 3453-generate-binary-strings-without-adjacent-zeros/generate-binary-strings-without-adjacent-zeros.java
@@ -0,0 +1,23 @@
+class Solution {
+    public List<String> validStrings(int n) {
+        ArrayList<String>list=new ArrayList<>();
+        int totalStringsLen=(int)Math.pow(2,n);
+        String binary;
+        for(int i=0;i<totalStringsLen;i++){
+            binary=Integer.toBinaryString(i);
+            while(binary.length()!=n){
+                binary="0"+binary;
+            }
+            boolean valid=true;
+            for(int j=1;j<n;j++){
+                if(binary.charAt(j)=='0' && binary.charAt(j-1)=='0'){
+                    valid=false;
+                }
+            }
+            if(valid){
+                list.add(binary);
+            }
+        }
+        return list;
+    }
+}
