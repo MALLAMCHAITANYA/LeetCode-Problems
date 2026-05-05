f9a42f1ef5cd99a64038d7b91b874a9e20158c06
Mallam Chaitanya <mallamchaitanya2005@gmail.com>
2026-05-05 08:13:00 +0530

    Time: 2 ms (99.36%) | Memory: 44.2 MB (83.95%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,16 +1,26 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        String correct="";
-        for(int i=0;i<s.length();i++){
-            char ch=s.charAt(i);
-            if(Character.isLetterOrDigit(ch)){
-                correct+=Character.toLowerCase(ch);
+        int l=0;
+        int r=s.length()-1;
+
+        while(l<r){
+            char left=s.charAt(l);
+            char right=s.charAt(r);
+
+            if(!Character.isLetterOrDigit(left)){
+                l++;
+            }
+            else if(!Character.isLetterOrDigit(right)){
+                r--;
+            }
+            else{
+                if(Character.toLowerCase(left)!=Character.toLowerCase(right)){
+                    return false;
+                }
+                l++;
+                r--;
             }
         }
-        String reversed="";
-        for(int i=correct.length()-1;i>=0;i--){
-            reversed+=correct.charAt(i);
-        }
-        return reversed.equals(correct);
+        return true;
     }
 }
