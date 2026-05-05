979cf0dda49d433594b0aaa397bcf614b89c3b4b
Mallam Chaitanya <mallamchaitanya2005@gmail.com>
2026-05-05 23:16:57 +0530

    Time: 5 ms (97.54%) | Memory: 42.8 MB (68.73%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.java
+++ 647-palindromic-substrings/palindromic-substrings.java
@@ -1,24 +1,50 @@
-class Solution {
-    public int countSubstrings(String s) {
+//BruteForce
+// class Solution {
+//     public int countSubstrings(String s) {
+//         int count=0;
+//         for(int i=0;i<s.length();i++){
+//             for(int j=i;j<s.length();j++){
+//                 if(isPalindrome(s,i,j)){
+//                     count++;
+//                 }
+//             }
+//         }
+//         return count;
+//     }
+
+//     private boolean isPalindrome(String s,int left,int right){
+//         while(left<right){
+//             if(s.charAt(left)!=s.charAt(right)){
+//                 return false;
+//             }
+//             left++;
+//             right--;
+//         }
+//         return true;
+//     }
+// }
+
+
+class Solution{
+    public int countSubstrings(String s){
+        int n=s.length();
         int count=0;
-        for(int i=0;i<s.length();i++){
-            for(int j=i;j<s.length();j++){
-                if(isPalindrome(s,i,j)){
-                    count++;
-                }
-            }
+
+        for(int i=0;i<n;i++){
+            count+=expand(s,i,i); //odd length
+            count+=expand(s,i,i+1); //even lenght
         }
         return count;
     }
 
-    private boolean isPalindrome(String s,int left,int right){
-        while(left<right){
-            if(s.charAt(left)!=s.charAt(right)){
-                return false;
-            }
-            left++;
-            right--;
+    private int expand(String s,int left, int right){
+        int count=0;
+
+        while(left>=0 && right<s.length() && s.charAt(left)==s.charAt(right)){
+            count++;
+            left--;
+            right++;
         }
-        return true;
+        return count;
     }
 }
