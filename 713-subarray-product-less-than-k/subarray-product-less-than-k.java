490b110975a1b2e912590620376ff3d927e2faba
Mallam Chaitanya <mallamchaitanya2005@gmail.com>
2026-05-07 07:17:32 +0530

    Time: 4 ms (99.96%) | Memory: 51.3 MB (38.75%) - LeetSync

--- 713-subarray-product-less-than-k/subarray-product-less-than-k.java
+++ 713-subarray-product-less-than-k/subarray-product-less-than-k.java
@@ -1,23 +1,45 @@
-class Solution {
-    public int numSubarrayProductLessThanK(int[] nums, int k) {
+// class Solution {
+//     public int numSubarrayProductLessThanK(int[] nums, int k) {
+//         if(k<=1){
+//             return 0;
+//         }
+//         int n=nums.length;
+//         int count=0;
 
+//         for(int i=0;i<n;i++){
+//             int prod=1;
+//             for(int j=i;j<n;j++){
+//                 prod*=nums[j];
+//                 if(prod<k){
+//                     count++;
+//                 }
+//                 else{
+//                     break;
+//                 }
+//             }
+//         }
+//         return count;
+//     }
+// }
+
+
+class Solution {
+    public int numSubarrayProductLessThanK(int[] nums, int k){
+        int n=nums.length;
+        int left=0;
+        int prod=1;
+        int count=0;
         if(k<=1){
             return 0;
         }
-        int n=nums.length;
-        int count=0;
-
-        for(int i=0;i<n;i++){
-            int prod=1;
-            for(int j=i;j<n;j++){
-                prod*=nums[j];
-                if(prod<k){
-                    count++;
-                }
-                else{
-                    break;
-                }
+        
+        for(int right=0;right<n;right++){
+            prod*=nums[right];
+            while(prod>=k){
+                prod/=nums[left];
+                left++;
             }
+            count+=right-left+1;
         }
         return count;
     }
