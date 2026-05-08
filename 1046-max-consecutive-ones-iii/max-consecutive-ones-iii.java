81da80f4a473144b16fa8c213435c055c5f73119
Mallam Chaitanya <mallamchaitanya2005@gmail.com>
2026-05-08 11:31:35 +0530

    Time: 3 ms (97.23%) | Memory: 52.2 MB (35.78%) - LeetSync

--- 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.java
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.java
@@ -19,46 +19,46 @@
 // }
 
 
-// class Solution{
-//     public int longestOnes(int[] nums, int k){
-//         int n=nums.length;
-//         int left=0;
-//         int count=0;
-//         int maxCount=0;
-
-//         for(int right=0;right<nums.length;right++){
-//             if(nums[right]==0){
-//                 count++;
-//             }
-//             while(count>k){
-//                 if(nums[left]==0){
-//                     count--;
-//                 }
-//                 left++;
-//             }
-//             maxCount=Math.max(maxCount,right-left+1);
-//         }
-//         return maxCount;
-//     }
-// }
-
 class Solution{
     public int longestOnes(int[] nums, int k){
+        int n=nums.length;
         int left=0;
         int count=0;
-        int n=nums.length;
+        int maxCount=0;
 
-        for(int right=0;right<n;right++){
+        for(int right=0;right<nums.length;right++){
             if(nums[right]==0){
                 count++;
             }
-            if(count>k){
+            while(count>k){
                 if(nums[left]==0){
                     count--;
                 }
                 left++;
             }
+            maxCount=Math.max(maxCount,right-left+1);
         }
-        return n-left;
+        return maxCount;
     }
 }
+
+// class Solution{
+//     public int longestOnes(int[] nums, int k){
+//         int left=0;
+//         int count=0;
+//         int n=nums.length;
+
+//         for(int right=0;right<n;right++){
+//             if(nums[right]==0){
+//                 count++;
+//             }
+//             if(count>k){
+//                 if(nums[left]==0){
+//                     count--;
+//                 }
+//                 left++;
+//             }
+//         }
+//         return n-left;
+//     }
+// }
