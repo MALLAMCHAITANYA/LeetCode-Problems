b0871f44a61d4a20d032d138baaa745fd0cd261f
Mallam Chaitanya <mallamchaitanya2005@gmail.com>
2026-06-05 22:43:47 +0530

    Time: 5 ms (85.16%) | Memory: 46.6 MB (53.50%) - LeetSync

--- 2639-separate-the-digits-in-an-array/separate-the-digits-in-an-array.java
+++ 2639-separate-the-digits-in-an-array/separate-the-digits-in-an-array.java
@@ -1,31 +1,23 @@
+import java.util.*;
+
 class Solution {
     public int[] separateDigits(int[] nums) {
-        ArrayList<Integer>list=new ArrayList<>();
-        for (int i = 0; i < nums.length; i++) {
-            int[] digits = check(nums[i]);
+        ArrayList<Integer> list = new ArrayList<>();
 
-            for (int d : digits) {
-                list.add(d);
+        for (int num : nums) {
+            String s = String.valueOf(num);
+
+            for (int i = 0; i < s.length(); i++) {
+                list.add(s.charAt(i) - '0');
             }
         }
-        int arr[]=new int[list.size()];
-        for(int i=0;i<list.size();i++){
-            arr[i]=list.get(i);
-        }
-        return arr;
-    }
 
-    private int []check(int n){
-        ArrayList<Integer>list=new ArrayList<>();
-        String s=n+"";
-        for(int i=0;i<s.length();i++){
-            list.add(s.charAt(i)-'0');
+        int[] ans = new int[list.size()];
+
+        for (int i = 0; i < list.size(); i++) {
+            ans[i] = list.get(i);
         }
 
-        int arr[]=new int[list.size()];
-        for(int i=0;i<list.size();i++){
-            arr[i]=list.get(i);
-        }
-        return arr;
+        return ans;
     }
 }
